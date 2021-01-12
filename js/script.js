6cdc9493062edecbc8e149678b4a67400e0b7138
Austin Landis <austinlandiscode@gmail.com>
2021-01-12 15:49:47 -0800

    Add js to allow our definitions to show up when we click on them.
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,59 @@
 $(document).ready(function() {
-
-})
+    $("#javascript").click(function() {
+        $("#javascriptanswer").fadeToggle();
+    });
+    $("#operators").click(function() {
+        $("#operatorsanswer").fadeToggle();
+    });
+    $("#variable").click(function() {
+        $("#variableanswer").fadeToggle();
+    });
+    $("#variable-naming-conventions").click(function() {
+        $("#variable-naming-conventionsanswer").fadeToggle();
+    });
+    $("#functions").click(function() {
+        $("#functionsanswer").fadeToggle();
+    });
+    $("#methods").click(function() {
+        $("#methodsanswer").fadeToggle();
+    });
+    $("#arguments").click(function() {
+        $("#argumentsanswer").fadeToggle();
+    });
+    $("#parameters").click(function() {
+        $("#parametersanswer").fadeToggle();
+    });
+    $("#return").click(function() {
+        $("#returnanswer").fadeToggle();
+    });
+    $("#chaining-methods").click(function() {
+        $("#chaining-methodsanswer").fadeToggle();
+    });
+    $("#strings").click(function() {
+        $("#stringsanswer").fadeToggle();
+    });
+    $("#booleans").click(function() {
+        $("#booleansanswer").fadeToggle();
+    });
+    $("#undefined").click(function() {
+        $("#undefinedanswer").fadeToggle();
+    });
+    $("#nan").click(function() {
+        $("#nananswer").fadeToggle();
+    });
+    $("#null").click(function() {
+        $("#nullanswer").fadeToggle();
+    });
+    $("#alert").click(function() {
+        $("#alertanswer").fadeToggle();
+    });
+    $("#comments").click(function() {
+        $("#commentsanswer").fadeToggle();
+    });
+    $("#jquery").click(function() {
+        $("#jqueryanswer").fadeToggle();
+    });
+    $("#attributes").click(function() {
+        $("#attributesanswer").fadeToggle();
+    });
+});
